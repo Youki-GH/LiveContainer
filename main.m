f49404410c81154b348b9ae6b302f6b624c0f6d5
Youki <youki0719new1@gmail.com>
2024-05-15 21:41:38 +0900

    update

--- main.m
+++ main.m
@@ -294,10 +294,21 @@ static void exceptionHandler(NSException *exception) {
 }
 
 int LiveContainerMain(int argc, char *argv[]) {
+    // Check if the application was launched from a quick action
+    UIApplicationShortcutItem *shortcutItem = [UIApplicationShortcutItem new];
+    if (argc > 1 && strcmp(argv[1], "UIApplicationShortcutItemLaunch") == 0 && argc > 2) {
+        NSString *shortcutType = [NSString stringWithUTF8String:argv[2]];
+        shortcutItem = [[UIApplicationShortcutItem alloc] initWithType:shortcutType localizedTitle:@"" localizedSubtitle:nil icon:nil userInfo:nil];
+    }
+    
+    // Handle the quick action
+    if (shortcutItem.type) {
+            [lcUserDefaults removeObjectForKey:@"selected"];
+    }
+
     lcUserDefaults = NSUserDefaults.standardUserDefaults;
     NSString *selectedApp = [lcUserDefaults stringForKey:@"selected"];
-    BOOL quickAction = [[NSUserDefaults standardUserDefaults] boolForKey:@"quick-action"];
-    if (selectedApp && quickAction == NO) {
+    if (selectedApp) {
         NSSetUncaughtExceptionHandler(&exceptionHandler);
         LCHomePath(); // init host home path
         NSString *appError = invokeAppMain(selectedApp, argc, argv);
