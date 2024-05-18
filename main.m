931a8c4ed9fff73e9b49d4b177c04fde3bd607b9
Youki <youki0719new1@gmail.com>
2024-05-18 18:54:42 +0900

    revert app id for release

--- main.m
+++ main.m
@@ -295,8 +295,8 @@ static void exceptionHandler(NSException *exception) {
 
 int LiveContainerMain(int argc, char *argv[]) {
     lcUserDefaults = NSUserDefaults.standardUserDefaults;
-    BOOL enableFeature = [lcUserDefaults boolForKey:@"deselect_app"];
-    if (enableFeature) {
+    BOOL deselectApp = [lcUserDefaults boolForKey:@"deselect_app"];
+    if (deselectApp) {
         [lcUserDefaults removeObjectForKey:@"selected"]; 
         [lcUserDefaults setBool:NO forKey:@"deselect_app"];
     }
